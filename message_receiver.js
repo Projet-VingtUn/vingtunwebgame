1fcb623c6616d0e48aaf93bdd5a0a561012930b8
KaboreIssouf <kaboreissouf.wk@gmail.com>
2025-12-16 18:46:36 +0000

    up

--- message_receiver.js
+++
@@ -1,6 +0,0 @@
-function sendMessageToUnity(unityInstance) {
-  var testObject = { token: "anonymous", scoreMin: 3, idParticipation: 0, env:"prod"};
-  var jsonString = JSON.stringify(testObject);
-  unityInstance.SendMessage('GameManager', 'UserLogin', jsonString);
-  console.log("Message Sended");
-}
